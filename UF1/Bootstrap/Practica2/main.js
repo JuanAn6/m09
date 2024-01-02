4d86935d5d6607f3c66f752bd4bd00e711ca4aed
JuanAn6 <juanantoniorizija666@gmail.com>
2024-01-02 22:43:15 +0100

    02-01-2024

--- UF1/Bootstrap/Practica2/main.js
+++ UF1/Bootstrap/Practica2/main.js
@@ -5,32 +5,34 @@ function f_main()
     document.getElementById('right').addEventListener('click', right);
     document.getElementById('left').addEventListener('click', left);
 }
-let selected = -1;
-const max_selected = 4;
+
 
 function right()
 {
     console.log('right');
-    document.getElementById('content').scrollLeft += 600;
+    document.getElementById('content').scrollLeft += 300;
     selected_game(1);
 }
 function left()
 {
     console.log('left');
-    document.getElementById('content').scrollLeft -= 600;
+    document.getElementById('content').scrollLeft -= 300;
     selected_game(-1);
 }
 
+let selected = -1;
+const max_selected = 4;
+
 function selected_game(num)
 {
+    console.log(selected);
     if(selected == -1){
         selected = 0;
-    }
-    console.log(selected);
-    if(selected + num >= 0 && selected + num < max_selected){
+        document.getElementById('game-'+selected).classList.add('selected-game');
+    }else if(selected + num >= 0 && selected + num < max_selected && selected != -1){
         document.getElementById('game-'+selected).classList.remove('selected-game');
         selected = selected + num;
         document.getElementById('game-'+selected).classList.add('selected-game');
-    }
-    
+    } 
+    console.log(selected);
 }
